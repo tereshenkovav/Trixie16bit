b32e1d3b07b1d0eaa3b5a5bd27c6afb0b212a1eb
TAV <tereshenkov@mail.ru>
2023-06-19 18:20:20 +0300

    Добавлен скрипт создания архива с DOSBox и языками.

--- setup/build.bat
+++ setup/build.bat
@@ -1,3 +1,5 @@
 "C:\Programs\NSIS\makensis.exe" trixie16bit.nsi
 
+call create_zip32.bat ru RU
+call create_zip32.bat en EN
 
---
+++ setup/create_zip32.bat
@@ -0,0 +1,18 @@
+if NOT "%~1" == "" goto mainproc
+
+echo "Argument - lang code" 
+exit
+
+:mainproc
+
+rm -f Trixie16bit-%2-1.0.0-DOSBox-Win32.zip
+7z a -mx9 Trixie16bit-%2-1.0.0-DOSBox-Win32.zip ..\bin
+cd dosbox
+7z a -mx9 ..\Trixie16bit-%2-1.0.0-DOSBox-Win32.zip *
+cd ..
+
+SET TMPDIR=%TEMP%\ZKJd62lksLsM3
+mkdir %TMPDIR%\bin\data
+echo %1 > %TMPDIR%\bin\data\deflang
+
+7z a -mx9 Trixie16bit-%2-1.0.0-DOSBox-Win32.zip %TMPDIR%\bin
